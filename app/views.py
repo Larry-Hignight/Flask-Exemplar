90ac50a33f566a8df1dacc9d24589db07754e7cb
Larry-Hignight <larry.hignight@gmail.com>
2017-09-15 16:27:08 -0700

    Incorporated the web templating example from Miguel's blog.

--- app/views.py
+++ app/views.py
@@ -2,14 +2,18 @@ from flask import render_template
 from app import app
 
 @app.route('/')
+def hello():
+    return "Hello, World!"
+
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Larry'} # mocked user
+    return render_template('index.html', title='Home', user=user)
 
 @app.route('/blog')
 def blog():
-    user = {'nickname': 'Miguel'}  # fake user
-    posts = [  # fake array of posts
+    user = {'nickname': 'Miguel'}  # mock user
+    posts = [                      # mock posts
         { 
             'author': {'nickname': 'John'}, 
             'body': 'Beautiful day in Portland!' 
@@ -19,7 +23,4 @@ def blog():
             'body': 'The Avengers movie was so cool!' 
         }
     ]
-    return render_template("blog.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
+    return render_template("blog.html", title='Home', user=user, posts=posts)
