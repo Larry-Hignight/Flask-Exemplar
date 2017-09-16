2d22148aa4706dd9d1b1eff90540d0ef745a5ee2
Larry-Hignight <larry.hignight@gmail.com>
2017-09-15 17:05:55 -0700

    Incorporated the Login code from part 3 of Miguel's Flask tutorial.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 def hello():
@@ -24,3 +25,21 @@ def blog():
         }
     ]
     return render_template("blog.html", title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form)
+
+@app.route('/login2', methods=['GET', 'POST'])
+def login2():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form)
