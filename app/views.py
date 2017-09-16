133359e7702f67681b88d208b122d8bcfecd73e9
Larry-Hignight <larry.hignight@gmail.com>
2017-09-15 17:19:55 -0700

    Revert "Incorporated the Login code from part 3 of Miguel's Flask tutorial."
    This reverts commit 2d22148aa4706dd9d1b1eff90540d0ef745a5ee2.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
-from flask import render_template, flash, redirect
+from flask import render_template
 from app import app
-from .forms import LoginForm
 
 @app.route('/')
 def hello():
@@ -25,21 +24,3 @@ def blog():
         }
     ]
     return render_template("blog.html", title='Home', user=user, posts=posts)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    return render_template('login.html', 
-                           title='Sign In',
-                           form=form)
-
-@app.route('/login2', methods=['GET', 'POST'])
-def login2():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
-    return render_template('login.html', 
-                           title='Sign In',
-                           form=form)
