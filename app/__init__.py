7eea1044bcf850d62226877c6cecfa4765843a5c
Larry-Hignight <larry.hignight@gmail.com>
2017-09-30 00:05:43 +0000

    Simplifying the Flask app structure to be more like the Flask tutorial.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-from app import views
+
+#app.config.from_object('config')
+#db = SQLAlchemy(app)
+
+db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12345678@mysql/information_schema'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+from app import views #, models
--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from flask import render_template, request, jsonify, Response
 from app import app
 
+from mysql.connector import connect
+cnx = connect(user='flask', password='12345678', host='mysql', database='flask_data')
+cursor = cnx.cursor()
+
+@app.route('/mysql')
+def mysql():
+    cursor.execute("select 'Hello World'")
+    rows = []
+    for row in cursor:
+        rows.append(row)
+    return rows[0]
+
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -19,13 +32,13 @@ def template1():
 def template2():
     user = {'nickname': 'Miguel'}  # mock user
     posts = [                      # mock posts
-        { 
-            'author': {'nickname': 'John'}, 
-            'body': 'Beautiful day in Portland!' 
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
         },
-        { 
-            'author': {'nickname': 'Susan'}, 
-            'body': 'The Avengers movie was so cool!' 
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
         }
     ]
     return render_template("blog.html", title='Home', user=user, posts=posts)
@@ -41,12 +54,8 @@ def show_user_profile(username):
 def show_post(post_id):
     return 'Post %d' % post_id
 
-#@app.route('/login', methods=['GET', 'POST'])
-#def login():
-#    if request.method == 'POST':
-#        do_the_login()
-#    else:
-#        show_the_login_form()
+
+## ---- JSON Response ---- ##
 
 @app.route('/test_json')
 def test_json():
@@ -56,13 +65,18 @@ def test_json():
     return jsonify(s)
 
 
+## ---- Database Tests ---- ##
+
+
+
 ## -- Static Files -- ##
+
 
 @app.route('/test_xml')
 def test_xml():
-    app.send_static_file('static/book.xml')
+    return app.send_static_file('static/book.xml')
 
-import os    
+import os
 @app.route('/test_image')
 def test_image():
     app.logger.error("CWD > " + os.getcwd())
@@ -75,4 +89,3 @@ def test_image():
 @app.route('/static')
 def test_static():
     return app.send_static_file('app/static/')
-
